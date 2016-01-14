08b62d680ae8e66c1b61812cc96e886ea9a872e5
Rian Quinn <quinnr@ainfosec.com>
2016-01-14 05:01:53 -0800

    Symbol Update
    Updated the code to provide as many symbols as possible. It's
    likely that more symbols need to be defined, but this should
    cover the bulk of them.
    
    Signed-off-by: Rian Quinn <quinnr@ainfosec.com>

--- bfc.c
+++ bfc.c
@@ -3,29 +3,104 @@
 #include <errno.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <sys/types.h>
+#include <sys/times.h>
 
-#ifndef ATEXIT_PROVIDED
-int
-atexit(void (*func)(void)) __THROW
+#include <regex.h>
+// Supported Symbols
+//
+// Note, these symbols were identified by extracting the object files from
+// newlib's libc.a, and then linking them together into a shared object using
+// ld. Finally, using readelf, you can grep for the undefined symbols, which
+// provides a list of all of the symbols that newlib needs you to provide. If
+// a symbol is missing, try the following:
+//
+// ~/opt/cross/bin/x86_64-elf-ar x libc.a
+// ~/opt/cross/bin/x86_64-elf-ld -shared -o libc.so *.o
+// readelf libc.so -aW | grep UND
+//
+// These are the symbols that we have identified that need to be implemented.
+// Note that there might be more depending on how you configure newlib to
+// compile, and they might be in a different order
+//
+// times
+// execve
+// getpid
+// isatty
+// lseek
+// _init
+// write
+// fstat
+// kill
+// wait
+// read
+// unlink
+// fork
+// sbrk
+// regcomp
+// gettimeofday
+// clock_gettime
+// regexec
+// _fini
+// stat
+// link
+// _exit
+// open
+// regfree
+// fcntl
+// mkdir
+// posix_memalign
+// close
+// sigprocmask
+// _malloc_r
+// _free_r
+// _calloc_r
+// _realloc_r
+
+#ifndef SYM_PROVIDED__TIMES
+clock_t
+times(struct tms *buf)
 {
-    (void) func;
+    (void) buf;
 
     return 0;
 }
 #endif
 
-#ifndef _EXIT_PROVIDED
-void
-_exit(int status)
+#ifndef SYM_PROVIDED__EXECVE
+int
+execve(const char *path, char *const argv[], char *const envp[])
 {
-    (void) status;
+    (void) path;
+    (void) argv;
+    (void) envp;
 
-    while(1);
+    errno = -ENOSYS;
+    return -1;
 }
 #endif
 
-#ifndef LSEEK_PROVIDED
+#ifndef SYM_PROVIDED__GETPID
+pid_t
+getpid(void)
+{
+    return 0;
+}
+#endif
+
+#ifndef SYM_PROVIDED__ISATTY
+int
+isatty(int fd)
+{
+    (void) fd;
+
+    errno = -ENOSYS;
+    return -1;
+}
+#endif
+
+#ifndef SYM_PROVIDED__LSEEK
 off_t
 lseek(int fd, off_t offset, int whence)
 {
@@ -38,15 +113,25 @@ lseek(int fd, off_t offset, int whence)
 }
 #endif
 
-#ifndef GETPID_PROVIDED
-pid_t
-getpid(void)
+#ifndef SYM_PROVIDED__INIT
+void _init(void)
 {
-    return 0;
 }
 #endif
 
-#ifndef FSTAT_PROVIDED
+#ifndef SYM_PROVIDED__WRITE
+_READ_WRITE_RETURN_TYPE
+write(int file, const void *buffer, size_t count)
+{
+    (void) file;
+    (void) buffer;
+
+    errno = -ENOSYS;
+    return -1;
+}
+#endif
+
+#ifndef SYM_PROVIDED__FSTAT
 int
 fstat(int file, struct stat *sbuf)
 {
@@ -58,42 +143,7 @@ fstat(int file, struct stat *sbuf)
 }
 #endif
 
-#ifndef CLOCK_GETTIME_PROVIDED
-int
-clock_gettime(clockid_t clk_id, struct timespec *tp) __THROW
-{
-    (void) clk_id;
-    (void) tp;
-
-    errno = -ENOSYS;
-    return -1;
-}
-#endif
-
-#ifndef OPEN_PROVIDED
-int
-open(const char *file, int mode, ...)
-{
-    (void) file;
-    (void) mode;
-
-    errno = -ENOSYS;
-    return -1;
-}
-#endif
-
-#ifndef CLOSE_PROVIDED
-int
-close(int fd)
-{
-    (void) fd;
-
-    errno = -ENOSYS;
-    return -1;
-}
-#endif
-
-#ifndef KILL_PROVIDED
+#ifndef SYM_PROVIDED__KILL
 int
 kill(pid_t _pid, int _sig)
 {
@@ -105,18 +155,18 @@ kill(pid_t _pid, int _sig)
 }
 #endif
 
-#ifndef ISATTY_PROVIDED
-int
-isatty(int fd)
+#ifndef SYM_PROVIDED__KILL
+pid_t
+wait(int *status)
 {
-    (void) fd;
+    (void) status;
 
     errno = -ENOSYS;
     return -1;
 }
 #endif
 
-#ifndef READ_PROVIDED
+#ifndef SYM_PROVIDED__READ
 _READ_WRITE_RETURN_TYPE
 read(int fd, void *buffer, size_t length)
 {
@@ -129,19 +179,216 @@ read(int fd, void *buffer, size_t length)
 }
 #endif
 
-#ifndef WRITE_PROVIDED
-_READ_WRITE_RETURN_TYPE
-write(int file, const void *buffer, size_t count)
+#ifndef SYM_PROVIDED__UNLINK
+int
+unlink(const char *file)
 {
     (void) file;
-    (void) buffer;
 
     errno = -ENOSYS;
     return -1;
 }
 #endif
 
-#ifndef MALLOC_PROVIDED
+#ifndef SYM_PROVIDED__FORK
+pid_t
+fork(void)
+{
+    errno = -ENOSYS;
+    return -1;
+}
+#endif
+
+#ifndef SYM_PROVIDED__SBRK
+void *
+sbrk(ptrdiff_t __incr)
+{
+    (void) __incr;
+
+    errno = -ENOSYS;
+    return (void *)-1;
+}
+#endif
+
+#ifndef SYM_PROVIDED__REGCOMP
+int
+regcomp(regex_t *preg, const char *regex, int cflags)
+{
+    (void) preg;
+    (void) regex;
+    (void) cflags;
+
+    return REG_NOMATCH;
+}
+#endif
+
+#ifndef SYM_PROVIDED__GETTIMEOFDAY
+int
+gettimeofday(struct timeval *tp, void *tzp)
+{
+    (void) tp;
+    (void) tzp;
+
+    errno = -ENOSYS;
+    return -1;
+}
+#endif
+
+#ifndef SYM_PROVIDED__CLOCK_GETTIME
+int
+clock_gettime(clockid_t clk_id, struct timespec *tp) __THROW
+{
+    (void) clk_id;
+    (void) tp;
+
+    errno = -ENOSYS;
+    return -1;
+}
+#endif
+
+#ifndef SYM_PROVIDED__REGEXEC
+int
+regexec(const regex_t *preg, const char *string,
+        size_t nmatch, regmatch_t pmatch[], int eflags)
+{
+    (void) preg;
+    (void) string;
+    (void) nmatch;
+    (void) pmatch;
+    (void) eflags;
+
+    return REG_NOMATCH;
+}
+#endif
+
+#ifndef SYM_PROVIDED__FINI
+void _fini(void)
+{
+}
+#endif
+
+#ifndef SYM_PROVIDED__STAT
+int
+stat(const char *pathname, struct stat *buf)
+{
+    (void) pathname;
+    (void) buf;
+
+    errno = -ENOSYS;
+    return -1;
+}
+#endif
+
+#ifndef SYM_PROVIDED__LINK
+int
+link(const char *oldpath, const char *newpath)
+{
+    (void) oldpath;
+    (void) newpath;
+
+    errno = -ENOSYS;
+    return -1;
+}
+#endif
+
+#ifndef SYM_PROVIDED__EXIT
+void
+_exit(int status)
+{
+    (void) status;
+
+    while(1);
+}
+#endif
+
+#ifndef SYM_PROVIDED__OPEN
+int
+open(const char *file, int mode, ...)
+{
+    (void) file;
+    (void) mode;
+
+    errno = -ENOSYS;
+    return -1;
+}
+#endif
+
+#ifndef SYM_PROVIDED__REGFREE
+void
+regfree(regex_t *preg)
+{
+    (void) preg;
+}
+#endif
+
+#ifndef SYM_PROVIDED__FCNTL
+int
+fcntl(int fd, int cmd, ...)
+{
+    (void) fd;
+    (void) cmd;
+
+    errno = -ENOSYS;
+    return -1;
+}
+#endif
+
+#ifndef SYM_PROVIDED__MKDIR
+int
+mkdir(const char *path, mode_t mode)
+{
+    (void) path;
+    (void) mode;
+
+    errno = -ENOSYS;
+    return -1;
+}
+#endif
+
+#ifndef SYM_PROVIDED__MKDIR
+int
+posix_memalign(void **memptr, size_t alignment, size_t size)
+{
+    (void) memptr;
+    (void) alignment;
+    (void) size;
+
+    return NULL;
+}
+#endif
+
+#ifndef SYM_PROVIDED__CLOSE
+int
+close(int fd)
+{
+    (void) fd;
+
+    errno = -ENOSYS;
+    return -1;
+}
+#endif
+
+#ifndef SYM_PROVIDED__SIGPROCMASK
+int
+sigprocmask(int how, const sigset_t *set, sigset_t *oldset)
+{
+    (void) how;
+    (void) set;
+    (void) oldset;
+
+    errno = -ENOSYS;
+    return -1;
+}
+#endif
+
+#ifdef MALLOC_PROVIDED
+
+void *_malloc_r(size_t size);
+void _free_r(void *ptr);
+void *_calloc_r(size_t nmemb, size_t size);
+void *_realloc_r(void *ptr, size_t size);
+
+#ifndef SYM_PROVIDED__MALLOC
 void *
 _malloc_r(size_t size)
 {
@@ -149,13 +396,17 @@ _malloc_r(size_t size)
 
     return NULL;
 }
+#endif
 
+#ifndef SYM_PROVIDED__FREE
 void
 _free_r(void *ptr)
 {
     (void) ptr;
 }
+#endif
 
+#ifndef SYM_PROVIDED__CALLOC
 void *
 _calloc_r(size_t nmemb, size_t size)
 {
@@ -164,7 +415,9 @@ _calloc_r(size_t nmemb, size_t size)
 
     return NULL;
 }
+#endif
 
+#ifndef SYM_PROVIDED__REALLOC
 void *
 _realloc_r(void *ptr, size_t size)
 {
@@ -178,31 +431,25 @@ _realloc_r(void *ptr, size_t size)
 void *
 malloc(size_t size)
 {
-    (void) size;
-
-    return NULL;
+    return _malloc_r(size);
 }
 
 void
 free(void *ptr)
 {
-    (void) ptr;
+    _free_r(ptr);
 }
 
 void *
 calloc(size_t nmemb, size_t size)
 {
-    (void) nmemb;
-    (void) size;
-
-    return NULL;
+    return _calloc_r(nmemb, size);
 }
 
 void *
 realloc(void *ptr, size_t size)
 {
-    (void) ptr;
-    (void) size;
-
-    return NULL;
+    return _realloc_r(ptr, size);
 }
+
+#endif
